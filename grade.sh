fb5f8eeb05626e7dce2250b6a7c0e54d8847b964
amey247 <82931448+amey247@users.noreply.github.com>
2023-02-23 17:03:02 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -3,3 +3,22 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+if [! -f "student-submission"]
+{
+    echo 'Wrong file. Please submit the correct file'
+}
+fi
+
+mkdir GraderFile
+cp student-submission GraderFile
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.19.2.jar *.java
+
+if [$? -neq 0]
+{
+    echo 'Unsuccessful compilation. Please change the file'
+}
+fi
+
